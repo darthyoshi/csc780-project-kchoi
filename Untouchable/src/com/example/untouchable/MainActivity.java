8cedf7000e951334b92d754ca08f1b8d65ace28c
kay.n.choi <kay.n.choi@3838ac3a-ed25-8feb-45be-5ea468b58024>
2014-03-12 03:01:42 +0000

    Created project, title screen. TODO: finish instruction panel, link to title screen.

---
+++ Untouchable/src/com/example/untouchable/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.untouchable;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends Activity {
+ 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+ 
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+ 
+        setContentView(R.layout.activity_main);
+    }
+    
+    public void onClick(View v) {
+    	
+    }
+}
